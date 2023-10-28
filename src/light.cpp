96729eaa048de1061ad8163393b7d14f1433e905
Conner Sommerfield <conner.sommerfield@gmail.com>
2023-10-28 11:53:41 -0700

    Clean Up

--- src/light.cpp
+++ src/light.cpp
@@ -1,12 +1,16 @@
 #include "light.hpp"
 #include <chrono>
+#include <thread>
+
+#define BLINK_PERIOD 200
 
 LightNode::LightNode() : Node("light")
 {
-    this->can_client_node = std::make_shared<rclcpp::Node>("raw_send_service_temp");
+    this->can_client_node = std::make_shared<rclcpp::Node>("send_can_raw_temp");
     this->can_client = can_client_node->create_client<scion_types::srv::SendFrame>("send_can_raw");
-    this->timer = this->create_wall_timer(std::chrono::milliseconds(200), [&](){
+    this->timer = this->create_wall_timer(std::chrono::milliseconds(BLINK_PERIOD), [&](){
         CanClient::turnOnLight(this->can_client_node, this->can_client);
+        std::this_thread::sleep_for(std::chrono::milliseconds(BLINK_PERIOD/2));
         CanClient::turnOffLight(this->can_client_node, this->can_client);
     });
 }
