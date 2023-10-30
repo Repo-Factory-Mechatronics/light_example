0ad7b80810e5ad8d0a8a3b9a22c99c1d72a9c872
Conner Sommerfield <conner.sommerfield@gmail.com>
2023-10-29 21:53:21 -0700

    Add timing

--- src/light.cpp
+++ src/light.cpp
@@ -3,14 +3,16 @@
 #include <thread>
 
 #define BLINK_PERIOD 200
+#define OFF 0
 
 LightNode::LightNode() : Node("light")
 {
     this->can_client_node = std::make_shared<rclcpp::Node>("send_can_raw_temp");
     this->can_client = can_client_node->create_client<scion_types::srv::SendFrame>("send_can_raw");
     this->timer = this->create_wall_timer(std::chrono::milliseconds(BLINK_PERIOD), [&](){
+        static bool lightState = OFF;
+        lightState ? CanClient::turnOffLight(this->can_client_node, this->can_client) :
         CanClient::turnOnLight(this->can_client_node, this->can_client);
-        std::this_thread::sleep_for(std::chrono::milliseconds(BLINK_PERIOD/2));
-        CanClient::turnOffLight(this->can_client_node, this->can_client);
+        lightState = !lightState;
     });
 }
